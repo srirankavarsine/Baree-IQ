c9b2ec409a776d1c14d25f0ee8375595d15ac49c
Srirankavarsine Bhupathe <vps2020srirankavarsine@gmail.com>
2026-08-19 20:52:29 +0530

    Delete app directory

--- app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .user import User
-from .skin_profile import SkinProfile
-from .product import Product
-from .recommendation import Recommendation
-
-__all__ = ["User", "SkinProfile", "Product", "Recommendation"]
